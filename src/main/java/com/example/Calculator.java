f85e10fc972d3a67cf3e461fa77bfcd13d05d8e3
Angela Gustafsson <gustafsson.angela@gmail.com>
2024-05-08 16:21:22 +0200

    Implemented step 4 plus code cleanup.

--- src/main/java/com/example/Calculator.java
+++ src/main/java/com/example/Calculator.java
@@ -6,9 +6,22 @@ import java.util.Arrays;
  * @author Angela Gustafsson, anggus-1
  */
 
+/**
+  Support different delimiters:
+
+ To change a delimiter, the beginning of the string will contain a separate line that looks like this:
+ “//[delimiter]\n[numbers…]” for example “//;\n1;2” should return three where the default delimiter is ‘;’.
+
+  The first line is optional. All existing scenarios should still be supported.
+ */
 public class Calculator {
     static int add(String numbers) {
-        String[] split = numbers.split(",|\n");
+        String defaultDelimiter =",|\n";
+        if (numbers.startsWith("//")){
+            defaultDelimiter = numbers.substring(2).split("\n")[0];
+            numbers = numbers.split("\n", 2)[1];
+        }
+        String[] split = numbers.split(defaultDelimiter);
         return Arrays.stream(split).filter(s -> !s.isEmpty()).map(Integer::parseInt).reduce(0,Integer::sum);
 
     }
--- src/test/java/com/example/CalculatorTest.java
+++ src/test/java/com/example/CalculatorTest.java
@@ -28,4 +28,8 @@ class CalculatorTest {
         assertEquals(6, Calculator.add("1\n2,3"));
 
     }
+    @Test
+    void step4newlineAndcomma() {
+        assertEquals(3, Calculator.add("//;\n1;2"));
+    }
 }
